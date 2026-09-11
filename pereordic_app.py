61833cc2ba32a651cd312bffe8c389fd95218358
407-39-Pimlapas <56713@yupparaj.ac.th>
2026-09-11 11:27:38 +0700

    Update pereordic_app.py

--- pereordic_app.py
+++ pereordic_app.py
@@ -57,14 +57,14 @@ def show_result_dialog(ans1, ans2):
     else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
-    )# ข้อ 2
+    # ข้อ 2
     if u_ans2 == "โซเดียม":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')"
     
-   )# ข้อ 2
+   # ข้อ 2
     if u_ans3 == "แคลเซียม":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
